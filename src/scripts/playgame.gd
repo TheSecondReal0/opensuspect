11e8ce0e48615924aad7817694cd30fdd2f35b42
darshadi <darshadi@protonmail.com>
2020-10-14 15:03:33 -0400

    add netcode

--- src/scripts/playgame.gd
+++ src/scripts/playgame.gd
@@ -1,22 +1,36 @@
 extends VBoxContainer
 
+export (NodePath) var Back = ""
+export (NodePath) var JoinGameDialog = ""
+export (NodePath) var HostnameField = ""
+export (NodePath) var PortField = ""
+export (NodePath) var CreatePortField = ""
+export (NodePath) var CreateGameDialog = ""
+export (String, FILE, "*.tscn") var main = ""
+
 func _ready():
-	$Back.connect("pressed", get_node(".."), "_on_Return")
+	Back = get_node(Back)
+	JoinGameDialog = get_node(JoinGameDialog)
+	HostnameField = get_node(HostnameField)
+	PortField = get_node(PortField)
+	CreatePortField = get_node(CreatePortField)
+	CreateGameDialog = get_node(CreateGameDialog)
+	Back.connect("pressed", get_node(".."), "_on_Return")
 
 func _on_JoinGame_pressed():
-	$JoinGameDialog.popup()
+	JoinGameDialog.popup()
 
 func _on_Connect_pressed():
 	Network.connection = Network.Connection.CLIENT
-	Network.host = $JoinGameDialog/JoinGameOptions/HostnameLine/HostnameField.text
-	Network.port = $JoinGameDialog/JoinGameOptions/Port/PortField.text
-	get_tree().change_scene("res://scenes/main.tscn")
+	Network.host = HostnameField.text
+	Network.port = PortField.text
+	get_tree().change_scene(main)
 
 func _on_Create_pressed():
 	Network.connection = Network.Connection.CLIENT_SERVER
 	Network.host = 'localhost'
-	Network.port = $CreateGameDialog/CreateGameOptions/PortLine/PortField.text
-	get_tree().change_scene("res://scenes/main.tscn")
+	Network.port = PortField.text
+	get_tree().change_scene(main)
 
 func _on_CreateGame_pressed():
-	$CreateGameDialog.popup()
+	CreateGameDialog.popup()
