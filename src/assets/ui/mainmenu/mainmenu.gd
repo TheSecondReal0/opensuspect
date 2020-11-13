bb0da457b1052dd62e660efadf1921fc81cd3a61
TheSecondReal0 <asarsprow@gmail.com>
2020-11-13 01:27:18 -0500

    WORKING

--- src/assets/ui/mainmenu/mainmenu.gd
+++ src/assets/ui/mainmenu/mainmenu.gd
@@ -5,9 +5,7 @@ signal returnToMainMenu
 
 func _ready() -> void:
 	default_state()
-	print("Loaded locales: ", TranslationServer.get_loaded_locales())
-	start_ngrok("D:/Downloads/ngrok.exe", 42420)
-	
+	print("Loaded locales: ", TranslationServer.get_loaded_locales())	
 
 func start_ngrok(ngrok_path: String, ngrok_port: int, ngrok_auth_token: String = ""):
 	match OS.get_name():
@@ -16,30 +14,36 @@ func start_ngrok(ngrok_path: String, ngrok_port: int, ngrok_auth_token: String =
 			var cmd
 			var output = []
 			if ngrok_auth_token == "":
-				cmd = OS.execute("CMD.exe", [str(ngrok_path) + " http " + str(ngrok_port)], false, output)
+				cmd = OS.execute("cmd", ["/k", str(ngrok_path) + " http " + str(ngrok_port)], false, output)
 			else:
-				cmd = OS.execute("CMD.exe", [str(ngrok_path) + " authtoken " + ngrok_auth_token, str(ngrok_path) + " http " + str(ngrok_port)], false)
-			print(cmd)
-			print(output)
+				cmd = OS.execute("cmd", ["/k", str(ngrok_path) + " authtoken " + ngrok_auth_token, str(ngrok_path) + " http " + str(ngrok_port)], false)
+			for i in output:
+				print(i)
 		"X11":
 			pass
 	#get url
 	print("getting url")
-	var ngrok_url: String = ""
-	var http_request = HTTPRequest.new()
-	add_child(http_request)
-	var test = http_request.connect("request_completed", self, "_on_request_completed")
-	print(test)
-	var error = http_request.request("http://localhost:4040/api/tunnels")#request("http://localhost:4040/api/tunnels/command_line")request("http://localhost:4040/api/tunnels")
-	print(error)
-
-func _on_request_completed(result, response_code, headers, body):
-	print("got url")
-	var json = JSON.parse(body.get_string_from_utf8())
-	print(result)
-	print(json.result)
+	var output = []
+	OS.execute("curl", ["http://localhost:4040/api/tunnels/command_line"], true, output)
+	while JSON.parse(output[0]).get_result().keys().has("error_code"):
+		#if output[0]["error_code"] != 100:
+		#	break
+		OS.execute("curl", ["http://localhost:4040/api/tunnels/command_line"], true, output)
+	var response = JSON.parse(output[0]).get_result()
+	for i in output:
+		print(i)
+	var ngrok_url = response.public_url
+	print(ngrok_url)
+	#var ngrok_url: String = ""
+	#var http_request = HTTPRequest.new()
+	#add_child(http_request)
+	#var test = http_request.connect("request_completed", self, "_on_request_completed")
+	#print(test)
+	#var error = http_request.request("http://localhost:4040/api/tunnels", [], true, HTTPClient.METHOD_GET)#request("http://localhost:4040/api/tunnels/command_line")request("http://localhost:4040/api/tunnels")
+	#print(error)
 
 func _on_NewGame_pressed() -> void:
+	start_ngrok("D:/Downloads/ngrok.exe", 42420)
 	show_only("PlayGame")
 
 func _on_Settings_pressed() -> void:
@@ -52,6 +56,7 @@ func default_state() -> void:
 	show_only("MainMenu")
 
 func show_only(element_name: String) -> void:
+	return
 	var element: Node = get_node(element_name)
 	for child in get_children():
 		child.visible = (child == element)
